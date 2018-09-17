a879661fe1da60e38ac1292234400a452cccb453
iarbaoui <ilyass.arbaoui@gmail.com>
2018-09-16 22:27:02 -0400

    Added Startup partial class

---
+++ Rqrsda/App_Start/Startup.OAuth.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rqrsda
+{
+    public partial class Startup
+    {
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
